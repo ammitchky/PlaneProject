028debb51e90d8a9d5d0d9edb45c26ba287f1985
Andrew Mitschke <ammitchky@gmail.com>
2021-05-11 12:32:30 -0500

    defined enum struct and class

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PlaneProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        enum SeatPosition {
+            Aisle,
+            Middle,
+            Window
+        }
+
+        public struct Seat {
+            string seatNumber;
+            SeatPosition seatPosition;
+            bool isAvailable;
+        }
+
+        public class Plane {
+            string tailNumber;
+            string flightNumber;
+            Seat[,] Seats = new Seat[10,6];
+        }
+    }
+}
