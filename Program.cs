14d865d0f3ac3fe0a18348d4da7416f679a24552
Robert Muehler <robertmuehler@gmail.com>
2021-05-11 14:29:12 -0400

    added PrintAvailible function, finalized project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace PlaneProject
 {
@@ -6,32 +7,71 @@ namespace PlaneProject
     {
         static void Main(string[] args)
         {
-            Plane plane = new Plane();
-            for (var i = 0; i < plane.Seats.GetLength(0); i++){
-
-                for (var j = 0; j < plane.Seats.GetLength(1); j++){
-                    var letter = (char)(97 + j);
-                    plane.Seats[i,j].seatNumber = $"{i+1}{letter}"; 
-                }
-            }
+            Plane plane = new Plane("", "");
+            plane.PrintAvailable();
         }
 
-        enum SeatPosition {
+        internal enum SeatPosition
+        {
             Aisle,
             Middle,
             Window
         }
 
-        public struct Seat {
-            public string seatNumber;
-            SeatPosition seatPosition;
-            bool isAvailable;
+        public struct Seat
+        {
+            public string SeatNumber { get; set; }
+            public SeatPosition SeatPosition { get; set; }
+            public bool IsAvailable { get; set; }
         }
 
-        public class Plane {
-            public string tailNumber;
-            public string flightNumber;
-            public Seat[,] Seats = new Seat[10,6];
+        public class Plane
+        {
+            string _tailNumber;
+            string _flightNumber;
+            private Seat[,] Seats = new Seat[10, 6];
+
+            public Plane(string tailNumber, string flightNumber)
+            {
+                this._tailNumber = tailNumber;
+                this._flightNumber = flightNumber;
+
+                for (int i = 0; i < this.Seats.GetLength(0); i++)
+                {
+                    for (int j = 0; j < this.Seats.GetLength(1); j++)
+                    {
+                        char letter = (char) (97 + j);
+                        this.Seats[i, j].SeatNumber = $"{i + 1}{letter}";
+
+                        if (j is 0 or 5)
+                        {
+                            this.Seats[i, j].SeatPosition = SeatPosition.Window;
+                            this.Seats[i, j].IsAvailable = true;
+                        }
+                        else if (j is 1 or 4)
+                        {
+                            this.Seats[i, j].SeatPosition = SeatPosition.Middle;
+                            this.Seats[i, j].IsAvailable = true;
+                        }
+                        else if (j is 2 or 3)
+                        {
+                            this.Seats[i, j].SeatPosition = SeatPosition.Aisle;
+                            this.Seats[i, j].IsAvailable = false;
+                        }
+                    }
+                }
+            }
+
+            public void PrintAvailable()
+            {
+                foreach (Seat seat in this.Seats)
+                {
+                    if (seat.IsAvailable)
+                    {
+                        Console.WriteLine(seat.SeatNumber + " - " + seat.SeatPosition);
+                    }
+                }
+            }
         }
     }
 }
